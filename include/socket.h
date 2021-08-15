a99e60983b97c2bca14c79d9f975835357af5dc2
17381792 <17381792@qq.com>
2021-08-16 00:40:45 +0800

    server创建（二）

---
+++ include/socket.h
@@ -0,0 +1,18 @@
+//
+// Created by wlh on 2021/8/16.
+//
+
+#ifndef PHP_7_4_15_SOCKET_H
+#define PHP_7_4_15_SOCKET_H
+
+#include "study.h"
+enum stSocket_type
+{
+    ST_SOCK_TCP          =  1,
+    ST_SOCK_UDP          =  2,
+};
+int stSocket_create(int type);
+
+int stSocket_bind(int sock, int type, char *host, int port);
+
+#endif //PHP_7_4_15_SOCKET_H
