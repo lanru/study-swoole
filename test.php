73d787842752a126a6dcf8c9c036be493f78bbc4
17381792 <17381792@qq.com>
2021-08-24 15:53:04 +0800

    添加php-xhprof-extension支持

--- test.php
+++ test.php
@@ -1,19 +1,5 @@
 <?php
 
-study_event_init();
-
 $chan = new Study\Coroutine\Channel();
 
-Sgo(function () use ($chan)
-{
-    $ret = $chan->pop();
-    var_dump($ret);
-});
-
-Sgo(function () use ($chan)
-{
-    $ret = $chan->push("hello world");
-    var_dump($ret);
-});
-
-study_event_wait();
+var_dump($chan);
